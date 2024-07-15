65bd7f5bd8243323acf29edc584bb1f5ed311c3f
quellus <quellus7@gmail.com>
2024-07-15 09:21:19 -0600

    Enemy avoider, add spawn points

--- enemy avoider/main.gd
+++ enemy avoider/main.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 var enemy = preload("res://Enemy.tscn")
-
+var rng = RandomNumberGenerator.new()
 
 
 
 func _on_enemy_spawn_timer_timeout():
+	var spawnpoints = get_tree().get_nodes_in_group("spawnpoint")
+	var spawn_index = rng.randf_range(0, 4)
 	var enemy_instance = enemy.instantiate()
 	add_child(enemy_instance)
+	enemy_instance.global_position = spawnpoints[spawn_index].global_position
