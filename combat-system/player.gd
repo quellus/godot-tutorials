9cb4898183663bc6e79451b75a7ec51a529d4f99
quellus <quellus7@gmail.com>
2025-11-23 07:46:41 -0700

    Update Combat System tutorial to Godot 4.5

--- combat-system/player.gd
+++ combat-system/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
+func _physics_process(_delta):
+	var direction = Input.get_vector(&"left", &"right", &"up", &"down")
 	if direction:
 		velocity = direction * SPEED
 	else:
